4638a7ddf8682422270d41f875cb6d05097de400
Ian Carrico <git@iancarrico.com>
2014-08-24 17:33:39 -0500

    Allow site to not use gh-pages branch anymore

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,28 @@
 
 var gulp = require('gulp');
 require('gulp-poole')(gulp);
+
+
+//////////////////////////////
+// Deploy Task
+//////////////////////////////
+gulp.task('deploy', function(cb) {
+  return runSequence(
+    'build',
+    'commit',
+    cb
+  );
+});
+
+//////////////////////////////
+// Publishing Task
+//////////////////////////////
+gulp.task('commit', function () {
+  gulp.src("./_site/**/*")
+    .pipe(deploy({
+      cacheDir: '.tmp',
+      branch: 'live'
+    })).pipe(gulp.dest('/tmp/gh-pages')); ;
+});
+
+}
