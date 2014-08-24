161921b6e2688a6a78ee2770266239d941875925
Ian Carrico <git@iancarrico.com>
2014-08-24 18:17:32 -0500

    Update 2014-08-24T23:17:32.354Z

--- header.php
+++ header.php
@@ -8,11 +8,28 @@ if (isset($_COOKIE['iamCSS'])) {
   $css = file_exists($path_to_check);
 }
 
+if (isset($_COOKIE['iamFonts'])) {
+  $fontpath = htmlspecialchars($_COOKIE['iamCSS'], ENT_QUOTES, 'UTF-8');
+  $font_path_to_check = ltrim($csspath, '/');
+  $fonts = file_exists($font_path_to_check);
+}
+
+if ($css || $fonts) {
+  echo file_get_contents('inlines/loadcss.inc');
+}
+
 if ($css) {
   echo '<link rel="stylesheet" href="' . $csspath . '">';
 }
 else {
   echo file_get_contents('inlines/style.inc');
+}
+
+if ($fonts) {
+  echo '<link rel="stylesheet" href="' . $fontpath . '">';
+}
+else {
+  echo file_get_contents('inlines/fonts.inc');
 }
 
 ?>
