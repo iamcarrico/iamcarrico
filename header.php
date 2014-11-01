44556f9c1c6514d9a31375181005223caff25c17
Ian Carrico <git@iancarrico.com>
2014-11-01 17:28:59 -0500

    Make the cookies smaller

--- header.php
+++ header.php
@@ -3,19 +3,25 @@
 $css = FALSE;
 $fonts = FALSE;
 
-if (isset($_COOKIE['iamCSS'])) {
-  $csspath = htmlspecialchars($_COOKIE['iamCSS'], ENT_QUOTES, 'UTF-8');
-  $path_to_check = ltrim($csspath, '/');
-  $css = file_exists($path_to_check);
+if (isset($_COOKIE['iamCSS']) || isset($_COOKIE['iamFonts'])) {
+  // Removing the old syntax.
+  echo '<script>document.cookie = "iamCSS=; expires=Fri Oct 31 2014 16:48:10 GMT-0500 (CDT)";document.cookie = "iamFonts=; expires=Fri Oct 31 2014 16:48:10 GMT-0500 (CDT)";</script>';
 }
 
-if (isset($_COOKIE['iamFonts'])) {
-  $fontpath = htmlspecialchars($_COOKIE['iamFonts'], ENT_QUOTES, 'UTF-8');
+if (isset($_COOKIE['iC'])) {
+  $csspath = htmlspecialchars($_COOKIE['iC'], ENT_QUOTES, 'UTF-8');
+  $path_to_check = ltrim($csspath, '/');
+  $css = file_exists("/assets/style-" . $path_to_check);
+}
+
+if (isset($_COOKIE['iF'])) {
+  $fontpath = htmlspecialchars($_COOKIE['iF'], ENT_QUOTES, 'UTF-8');
   $font_path_to_check = ltrim($fontpath, '/');
-  $fonts = file_exists($font_path_to_check);
+  $fonts = file_exists("/assets/fonts/" . $font_path_to_check);
 }
 
 if (!$css || !$fonts) {
+  header('x-do-not-cache');
   echo file_get_contents('inlines/loadcss.inc');
 }
 
