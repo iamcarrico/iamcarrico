c909bc831cb404b9d9bd747bdf0fb54b7b390b17
Ian Carrico <git@iancarrico.com>
2015-05-05 16:52:51 -0700

    Remove all compass extensions

--- config.rb
+++ config.rb
@@ -1,7 +1,4 @@
 # Require any additional compass plugins here.
-require 'singularitygs'
-require 'breakpoint'
-require 'toolkit'
 
 # Set this to the root of your project when deployed:
 http_path = "./"
