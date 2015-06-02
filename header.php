3ddb437a3a8306def5be7c8abc847509c37c1a85
Ian Carrico <git@iancarrico.com>
2015-06-02 10:07:48 -0400

    Remove includes and inlines we can do a better way now

--- header.php
+++ header.php
@@ -3,9 +3,6 @@
 $css = FALSE;
 $fonts = FALSE;
 
-// Just going to leave this here for a while.
-echo '<script>document.cookie = "iamCSS=; expires=Fri Oct 31 2011 16:48:10 GMT-0500 (CDT)";document.cookie = "iamFonts=; expires=Fri Oct 31 2011 16:48:10 GMT-0500 (CDT)";</script>';
-
 if (isset($_COOKIE['iC'])) {
   $csspath = "/assets/style-" . htmlspecialchars($_COOKIE['iC'], ENT_QUOTES, 'UTF-8');
   $path_to_check = ltrim($csspath, '/');
@@ -18,22 +15,10 @@ if (isset($_COOKIE['iF'])) {
   $fonts = file_exists($font_path_to_check);
 }
 
-if (!$css || !$fonts) {
-  header('x-do-not-cache');
-  echo file_get_contents('inlines/loadcss.inc');
-}
-
 if ($css) {
   echo '<link rel="stylesheet" href="' . $csspath . '">';
 }
 else {
   echo file_get_contents('inlines/css.inc');
-}
-
-if ($fonts) {
-  echo '<link rel="stylesheet" href="' . $fontpath . '">';
-}
-else {
-  echo file_get_contents('inlines/fonts.inc');
 }
 ?>
