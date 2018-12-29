5e9ff783f530946bc933749f79f1a47602ebe0f6
Ian Carrico <git@iancarrico.com>
2018-12-28 20:08:49 -0600

    I know, it is slower— but cheaper

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,3 @@
 
 var gulp = require('gulp');
 require('gulp-poole')(gulp);
-
-//////////////////////////////
-// Deploy Task
-// Because we dont want to deploy to gh-pages.
-//////////////////////////////
-gulp.task('deploy', ['build'])
--- js/footer.js
+++ js/footer.js
@@ -17,11 +17,9 @@
   Promise.all(fontPromises).then(function() {
     // If all promises are fulfilled, then add the proper class to signify.
     document.documentElement.className += " fonts-loaded";
-    document.cookie = "iF=true";
   }, function() {
     // A font did not load, create a class so that we know that we have
     // failed as loaders of fonts.
     document.documentElement.className += " fonts-unavailable";
-    document.cookie = "iF=false";
   });
 })();
