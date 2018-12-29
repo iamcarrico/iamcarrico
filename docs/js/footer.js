591695b160f98a34765dbff5ec6a7bd4838de5af
Ian Carrico <git@iancarrico.com>
2018-12-28 20:42:33 -0600

    Just make a docs folder, cuz that is easy

---
+++ docs/js/footer.js
@@ -0,0 +1,25 @@
+//= require vendor/fontfaceobserver
+//= require vendor/fastclick
+
+(function() {
+  'use strict';
+
+  // Enable fastclick.
+  FastClick.attach(document.body);
+
+  var fontPromises = [];
+  var pt = new FontFaceObserver('pt');
+  var italic = new FontFaceObserver('pt', {style: 'italic'});
+
+  fontPromises.push(pt.check(null, 5000));
+  fontPromises.push(italic.check(null, 5000));
+
+  Promise.all(fontPromises).then(function() {
+    // If all promises are fulfilled, then add the proper class to signify.
+    document.documentElement.className += " fonts-loaded";
+  }, function() {
+    // A font did not load, create a class so that we know that we have
+    // failed as loaders of fonts.
+    document.documentElement.className += " fonts-unavailable";
+  });
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,13 @@
 
 var gulp = require('gulp');
 require('gulp-poole')(gulp);
+
+var htmlmin = require('gulp-htmlmin')
+
+gulp.task('htmlmin', function() {
+  return gulp.src('./docs/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./docs/'))
+});
+
+gulp.task('deploy', ['build']);
