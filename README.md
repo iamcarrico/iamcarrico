47e5033c8fc6a71259912f16c9ce77ebcf2636df
Ian Carrico <git@iancarrico.com>
2026-03-21 12:17:37 -0400

    prior edits

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Welcome to the code repository for my blog. It is a specialty Jekyll site, with my own little flair mixed on it.
 
 
+## Development
+
+```npm run start```
+
+
 ## License
 
 The code within this repository is under the MIT License, while the content (e.g. blog posts) is under the Creative Commons Attribution-ShareAlike 4.0 International ([CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)).
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,21 @@ var gulp = require('gulp');
 require('gulp-poole')(gulp);
 
 var htmlmin = require('gulp-htmlmin')
+var browserSync = require('browser-sync');
 
 gulp.task('htmlmin', function() {
   return gulp.src('./docs/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./docs/'))
+});
+
+
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'docs'
+    }
+  });
 });
 
 gulp.task('deploy', ['build']);
